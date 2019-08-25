07cda549c9f8af1680ef3c16337baa5749185567
Rubén Murga <ruben.murga.d@gmail.com>
2019-08-25 02:41:35 -0500

    alert

--- src/routes/sms.js
+++ src/routes/sms.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 const {wrap} = require('./../util/wrap')
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
+var dateFormat = require('dateformat');
 
 router.route('/')
   .post(wrap(async (req, res) => {
@@ -12,37 +12,41 @@ router.route('/')
     console.log('a message just arrived.')
     //const twiml = new MessagingResponse()
     //console.log(twiml.toString(), twiml.message())
-    let data = []
-    await firebase.collection('ubicaciones').doc('b0YZVOXVr4N7Z03whP0I').get()
+    let data = {ubicacion : []}
+    await firebase.collection('ubicaciones').doc(req.body.Body.id).get()
     .then((snapshot) => {
       console.log(snapshot.data())
-      if (snapshot.data() != undefined || snapshot.data().length == 0){
-        console.log('hola1')
-        data.push({
+      if (snapshot.data() == undefined || snapshot.data().ubicacion.length == 0){
+        data.ubicacion.push({
           lat: req.body.Body.lat,
-          long: req.body.Body.lng
+          long: req.body.Body.lng,
+          time: new Date()
         })
-        console.log(data)
+        
       }
       else {
-        console.log('hola 2')
         data = snapshot.data()
-        data.push({
+        data.ubicacion.push({
           lat: req.body.Body.lat,
-          long: req.body.Body.long
+          long: req.body.Body.lng,
+          time: new Date()
         })
       }
     })
     .catch((err) => {
       console.log(err)
     });
-
-    let alerts = firebase.collection('ubicaciones').doc('b0YZVOXVr4N7Z03whP0I');
-
-    let setAda = alerts.set({
-      ubicacion: data
-    });
-    res.send({'message' : 'this is a message'})
+    let ubication = firebase.collection('ubicaciones').doc(req.body.Body.id);
+    await ubication.set(data);
+    let id = req.body.Body.id + dateFormat(new Date(), "yyyymmddhMMss");
+    console.log(id)
+    let alerta = firebase.collection('alertas').doc(id)
+    await alerta.set({
+      id: req.body.Body.id,
+      time: new Date(),
+      type: req.body.Body.type
+    })
+    res.send({'id' : id})
   }))
 
 module.exports = router
